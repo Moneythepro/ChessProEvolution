6debdd72e461853e5ef053409a658304dcd9ad33
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-31 21:33:38 +0530

    Create stockfish-worker.js

---
+++ stockfish-worker.js
@@ -0,0 +1,18 @@
+// stockfish-worker.js
+self.importScripts("stockfish.js");
+
+let engine = null;
+
+if (typeof Stockfish === "function") {
+  engine = Stockfish();
+} else {
+  engine = self.Module;
+}
+
+engine.onmessage = function (e) {
+  self.postMessage(e.data ? e.data : e);
+};
+
+self.onmessage = function (e) {
+  engine.postMessage(e.data);
+};
