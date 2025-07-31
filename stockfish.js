16d943ae6962207fc00aad9c377ec7662c7f1843
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-31 18:40:47 +0530

    Update stockfish.js

--- stockfish.js
+++ stockfish.js
@@ -1,1 +1,13 @@
+importScripts('https://cdn.jsdelivr.net/gh/niklasf/stockfish.wasm@master/src/stockfish.js');
 
+onmessage = function (event) {
+  postMessage('uci'); // Ensure UCI mode is active
+
+  const stockfish = new Worker('https://cdn.jsdelivr.net/gh/niklasf/stockfish.wasm@master/src/stockfish.js');
+
+  stockfish.onmessage = function (e) {
+    postMessage(e.data);
+  };
+
+  stockfish.postMessage(event.data);
+};
