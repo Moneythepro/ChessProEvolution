a803c13de3acea04300c5ef65d9404150a9295d6
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-08-03 10:12:36 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,48 @@
-const CACHE_NAME = "chess-v1";
+const CACHE_NAME = "chess-v2";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./stockfish.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./pieces/wp.svg",
+  "./pieces/wr.svg",
+  "./pieces/wn.svg",
+  "./pieces/wb.svg",
+  "./pieces/wq.svg",
+  "./pieces/wk.svg",
+  "./pieces/bp.svg",
+  "./pieces/br.svg",
+  "./pieces/bn.svg",
+  "./pieces/bb.svg",
+  "./pieces/bq.svg",
+  "./pieces/bk.svg",
+  "./win.mp3",
+  "./draw.mp3",
+  "./move.mp3"
 ];
 
+// Install event – cache essential files
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
+// Fetch event – serve from cache first
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
+// Activate event – clean old caches
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
   );
 });
